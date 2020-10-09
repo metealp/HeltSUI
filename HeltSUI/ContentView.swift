3099a0c6f1eb141c6fe8ac4c2604859ba367b391
Mete Alp Kızılçay <kizilcaymetealp@gmail.com>
2020-10-09 22:48:15 +0300

    Initial Commit

---
+++ HeltSUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HeltSUI
+//
+//  Created by Mete Alp Kizilcay on 9.10.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HeltSUI/HeltSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeltSUIApp.swift
+//  HeltSUI
+//
+//  Created by Mete Alp Kizilcay on 9.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct HeltSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
